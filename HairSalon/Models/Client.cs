db555b53939201420e40af935dd683e86b60d629
Joey Palchak <joey.palchak@gmail.com>
2023-10-06 08:22:17 -0700

    create Client class + Stylist class

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -3,6 +3,12 @@ namespace HairSalon.Models
 {
   public class Client
   {
-    
+    public int ClientId { get; set; }
+    public string Name { get; set; }
+
+    // Foreign Key
+    public int StylistId { get; set; }
+    // Reference Navigation Property
+    public Stylist Stylist {get; set; }
   }
 }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -1,8 +1,14 @@
+using System.Collections.Generic;
 
 namespace HairSalon.Models
 {
   public class Stylist
   {
-    
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public string Specialty { get; set; }
+
+    // Collection Navigation Property
+    public List<Client> Clients { get; set; }
   }
 }
