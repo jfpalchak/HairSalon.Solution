2239c764d17e841e2a43c6215b651dd336290ae1
Joey Palchak <joey.palchak@gmail.com>
2023-10-06 13:25:57 -0700

    remove comments from HomeController

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -7,13 +7,6 @@ namespace HairSalon.Controllers
 {
   public class HomeController : Controller
   {
-    // private readonly HairSalonContext _db;
-
-    // public HomeController(HairSalonContext db)
-    // {
-    //   _db = db;
-    // }
-
     public ActionResult Index()
     {
       return View();
