bc2853b5d22d84165bfbfd7b56582090e990e10e
Joey Palchak <joey.palchak@gmail.com>
2023-10-06 09:19:50 -0700

    add Clients/Index view + update Controller route

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using HairSalon.Models;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,7 +17,10 @@ namespace HairSalon.Controllers
 
     public ActionResult Index()
     {
-      return View();
+      List<Client> model = _db.Clients
+                              .Include(client => client.Stylist)
+                              .ToList();
+      return View(model);
     }
   }
 }
