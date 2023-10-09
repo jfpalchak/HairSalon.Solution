1a78a18d355158a950f977c68c9ef96c011c7202
Joey Palchak <joey.palchak@gmail.com>
2023-10-08 20:09:52 -0700

    update Client fields + Create & Edit forms

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -5,6 +5,8 @@ namespace HairSalon.Models
   {
     public int ClientId { get; set; }
     public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
 
     // Foreign Key
     public int StylistId { get; set; }
